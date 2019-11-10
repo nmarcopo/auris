f1fef2e92c9ae2aedb4a0dddf2df1eeec5df4bdf
Nick Marcopoli <nick@marcopo.li>
2019-11-10 06:56:31 -0500

    formatting

--- js/app.js
+++ js/app.js
@@ -6,21 +6,21 @@ var rec; 							//Recorder.js object
 var input; 							//MediaStreamAudioSourceNode we'll be recording
 var imgButton = document.getElementById("mic_image");
 var isRec = false;
-var notificationStrings = ["Dog","Police car (siren)","Siren","Ambulance (siren)","Emergency vehicle","Fire alarm", "Alarm", "Car alarm", "Bicycle bell"];
+var notificationStrings = ["Dog", "Police car (siren)", "Siren", "Ambulance (siren)", "Emergency vehicle", "Fire alarm", "Alarm", "Car alarm", "Bicycle bell"];
 var recentNotif = {};
 
-function onLoad(){
-  imgButton.addEventListener('click', function(){
-    if(isRec){
-      imgButton.src = "img/mic_off.png";
-      isRec=false;
-    }
-    else{
-      imgButton.src = "img/mic_on.png";
-      isRec=true;
-    }
-  });
-  recordAudio();
+function onLoad() {
+    imgButton.addEventListener('click', function () {
+        if (isRec) {
+            imgButton.src = "img/mic_off.png";
+            isRec = false;
+        }
+        else {
+            imgButton.src = "img/mic_on.png";
+            isRec = true;
+        }
+    });
+    recordAudio();
 }
 
 // shim for AudioContext when it's not avb. 
@@ -41,71 +41,71 @@ function sleep(ms) {
 async function recordAudio() {
     lock = false
     navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-    .then(function (stream) {
-        audioContext = new AudioContext();
-        analyser = audioContext.createAnalyser();
-        microphone = audioContext.createMediaStreamSource(stream);
-        javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+        .then(function (stream) {
+            audioContext = new AudioContext();
+            analyser = audioContext.createAnalyser();
+            microphone = audioContext.createMediaStreamSource(stream);
+            javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
 
-        analyser.smoothingTimeConstant = 0.8;
-        analyser.fftSize = 1024;
+            analyser.smoothingTimeConstant = 0.8;
+            analyser.fftSize = 1024;
 
-        microphone.connect(analyser);
-        analyser.connect(javascriptNode);
-        javascriptNode.connect(audioContext.destination);
-        javascriptNode.onaudioprocess = async function () {
-            var array = new Uint8Array(analyser.frequencyBinCount);
-            analyser.getByteFrequencyData(array);
-            var values = 0;
+            microphone.connect(analyser);
+            analyser.connect(javascriptNode);
+            javascriptNode.connect(audioContext.destination);
+            javascriptNode.onaudioprocess = async function () {
+                var array = new Uint8Array(analyser.frequencyBinCount);
+                analyser.getByteFrequencyData(array);
+                var values = 0;
 
-            var length = array.length;
-            for (var i = 0; i < length; i++) {
-                values += (array[i]);
-            }
-
-            soundLevel = values / length;
-
-            if(soundLevel > 70){
-                console.log(Math.round(soundLevel));
-            }
-            if (soundLevel > 10 && !lock && isRec) {
-                lock = true
-                startRecording()
-                await sleep(2500)
-                if(!isRec){
-                  rec.stop();
-
-                  //stop microphone access
-                  gumStream.getAudioTracks()[0].stop();
-                  lock=false;
-                  return;
+                var length = array.length;
+                for (var i = 0; i < length; i++) {
+                    values += (array[i]);
                 }
-                stopRecording();
-                lock = false
+
+                soundLevel = values / length;
+
+                if (soundLevel > 70) {
+                    console.log(Math.round(soundLevel));
+                }
+                if (soundLevel > 10 && !lock && isRec) {
+                    lock = true
+                    startRecording()
+                    await sleep(2500)
+                    if (!isRec) {
+                        rec.stop();
+
+                        //stop microphone access
+                        gumStream.getAudioTracks()[0].stop();
+                        lock = false;
+                        return;
+                    }
+                    stopRecording();
+                    lock = false
+                }
+                // colorPids(average);
             }
-            // colorPids(average);
-        }
-    })
-    .catch(function (err) {
-        /* handle the error */
-        console.log(err)
-    });
+        })
+        .catch(function (err) {
+            /* handle the error */
+            console.log(err)
+        });
 }
-async function notify(notificationStr){
-    if(!lock2){ 
-       lock2 = true;
-       console.log("I HAVE THE LOCK!!!!!!");
-       navigator.vibrate(500);
-    Notification.requestPermission().then(function (permission) {
-         // If the user accepts, let's create a notification
-         //if (permission === "granted") {
-             var notification = new Notification("Notifications are enabled!");
-             setTimeout(notification.close.bind(notification), 3000);
-         //}
-     });
-       await sleep(10000);
-       lock2 = false;
-   }
+async function notify(notificationStr) {
+    if (!lock2) {
+        lock2 = true;
+        console.log("I HAVE THE LOCK!!!!!!");
+        navigator.vibrate(500);
+        Notification.requestPermission().then(function (permission) {
+            // If the user accepts, let's create a notification
+            //if (permission === "granted") {
+            var notification = new Notification("Notifications are enabled!");
+            setTimeout(notification.close.bind(notification), 3000);
+            //}
+        });
+        await sleep(10000);
+        lock2 = false;
+    }
 }
 
 function startRecording() {
@@ -215,9 +215,9 @@ function displayPrediction(prediction) {
         li.innerText = element.label
         li.classList.add("list-group-item")
         c.appendChild(li);
-    if(notificationStrings.includes(element.label)){
-        notify(element.label);
-    }
+        if (notificationStrings.includes(element.label)) {
+            notify(element.label);
+        }
     });
     navigator.vibrate([500]);
     const p = document.createElement('div')
